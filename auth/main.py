c3e45c6e676ac8ce74b08cb21a1e00551931e204
max <maximalist000@gmail.com>
2026-06-28 00:19:57 +0300

    наброски валидации для регистрации

--- auth/main.py
+++ auth/main.py
@@ -1,7 +1,14 @@
+import re
+
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
+from pydantic import BaseModel, Field, BeforeValidator, AfterValidator, model_validator
+from typing import Annotated
+from typing_extensions import Self
+
+email_pattern = re.compile("^[^@]+@[^@]+\.[^@]{2,}$")
 
 metadata_obj = MetaData()
 
@@ -31,8 +38,37 @@ engine = create_engine(
 )
 
 
+def truncate(value: any) -> str:
+  if (isinstance(value, str)):
+    return value.strip()
+  
+  raise ValueError(f'{value} is not a string')
+
+
+def check_email(value: str) -> str:
+  if (email_pattern.match(value)):
+    return value
+
+  raise ValueError(f'{value} is not a valid email')
+
+
+class SignupBody(BaseModel):
+  fullname: Annotated[str, Field(min_length=3), BeforeValidator(truncate)]
+  email: Annotated[str, Field(min_length=4), BeforeValidator(truncate), AfterValidator(check_email)]
+  password: Annotated[str, Field(min_length=8), BeforeValidator(truncate)]
+  password_repeat: Annotated[str, Field(min_length=8), BeforeValidator(truncate)]
+
+  @model_validator(mode='after')
+  def compare_passwords(self) -> Self:
+    if self.password != self.password_repeat:
+      raise ValueError(f'password is not equal to password_repeat')
+    
+    return self
+
+
 @app.post("/signup")
-async def signup():
+async def signup(form: SignupBody):
+  user_dict = form.model_dump()
   return {"message": "Hello World"}
 
 
