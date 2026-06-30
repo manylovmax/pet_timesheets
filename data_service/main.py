370f10ebfdb7a12a25059bff6b2464fffc9e9f29
max <maximalist000@gmail.com>
2026-07-01 01:41:32 +0300

    data_service убрал ненужный импорт

--- data_service/main.py
+++ data_service/main.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-from datetime import datetime, date as Date
+from datetime import date as Date
 from fastapi import FastAPI
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
