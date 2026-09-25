01310dfd1b82e70df0ecebab49a477a0e61b4730
Maxim Manylov <maximalist000@gmail.com>
2026-09-25 19:47:58 +0300

    frontend/angular: валидация для форм логина и регистрации

--- frontend/angular/src/app/components/SigninForm/SigninForm.component.ts
+++ frontend/angular/src/app/components/SigninForm/SigninForm.component.ts
@@ -3,20 +3,36 @@ import { InputComponent } from "../Input/Input.component";
 import AuthService from "../../services/auth.service";
 import { RouterLink } from "@angular/router";
 import { Router } from '@angular/router';
+import { form, FormField, required } from "@angular/forms/signals";
+import { StatefulInput } from "../StatefulInput/StatefulInput.component";
+
+
+interface SigninFormModel {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'SigninForm',
   templateUrl: './SigninForm.component.html',
-  imports: [InputComponent, RouterLink],
+  imports: [RouterLink, StatefulInput, FormField],
 })
 export class SigninForm {
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
-  email = signal<string>('');
-  password = signal<string>('');
+
+
+  formModel = signal<SigninFormModel>({email: '', password: ''});
+  form = form(this.formModel, (schemaPath) => {
+    required(schemaPath.email, {message: 'Email is required'});
+    required(schemaPath.password, {message: 'Password is required'});
+  });
 
   async login(): Promise<void> {
-    if (await this.authService.signin(this.password(), this.email()))
+    if (this.form().invalid())
+      return;
+    
+    if (await this.authService.signin(this.formModel().password, this.formModel().email))
       this.router.navigate(['/timetable'])
   }
 }
--- frontend/angular/src/app/components/SignupForm/SignupForm.component.ts
+++ frontend/angular/src/app/components/SignupForm/SignupForm.component.ts
@@ -2,25 +2,55 @@ import { Component, inject, signal, Signal } from "@angular/core";
 import { InputComponent } from "../Input/Input.component";
 import AuthService from "../../services/auth.service";
 import { Router, RouterLink } from "@angular/router";
+import { form, FormField, required, validate } from "@angular/forms/signals";
+import { StatefulInput } from "../StatefulInput/StatefulInput.component";
+
+
+
+interface SignupFormModel {
+  email: string;
+  fullname: string;
+  password: string;
+  confirmPassword: string;
+}
+
 
 @Component({
   selector: 'SignupForm',
   templateUrl: './SignupForm.component.html',
-  imports: [InputComponent, RouterLink],
+  imports: [FormField, RouterLink, StatefulInput],
 })
 export class SignupForm {
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
-  email = signal<string>('');
-  password = signal<string>('');
-  passwordRepeat = signal<string>('');
-  fullname = signal<string>('');
+
+  formModel = signal<SignupFormModel>({email: '', password: '', confirmPassword: '', fullname: ''});
+  form = form(this.formModel, (schemaPath) => {
+    required(schemaPath.email, {message: 'Email is required'});
+    required(schemaPath.fullname, {message: 'Fullname is required'});
+    required(schemaPath.password, {message: 'Password is required'});
+    required(schemaPath.confirmPassword, {message: 'Please confirm your password'});
+     validate(schemaPath.confirmPassword, ({value, valueOf}) => {
+      const confirmPassword = value();
+      const password = valueOf(schemaPath.password);
+      if (confirmPassword !== password) {
+        return {
+          kind: 'passwordMismatch',
+          message: 'Passwords do not match',
+        };
+      }
+      return null;
+    });
+  });
 
   async signup(): Promise<void> {
+    if (this.form().invalid())
+      return;
+
     if (await this.authService.signup({
-      password: this.password(), 
-      email: this.email(),
-      fullname: this.fullname(),
+      password: this.formModel().password, 
+      email: this.formModel().email,
+      fullname: this.formModel().fullname,
     }))
       this.router.navigate(['/timetable'])
   }
